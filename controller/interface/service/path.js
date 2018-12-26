422fa3b3a49362d04d4e556a4943ded5f174cf41
Joaquin Osvaldo Rodriguez <meritoki@yahoo.com>
2018-12-25 21:09:53 -0300

    Fixing controller interfaces that do not exist

--- controller/interface/service/path.js
+++ controller/interface/service/path.js
@@ -1,5 +1,4 @@
 var properties = require('../../properties.js');
-var user = require('../user.js');
 
 exports.delete = function(router) {
   console.log('service.path.delete()');
@@ -11,6 +10,4 @@ exports.get = function(router) {
 
 exports.post = function(router) {
   console.log('service.path.post()');
-  router.post("/v1/auth/name/password", user.getNamePasswordUser);
-  router.post("/v1/auth/name", user.getNameUser);
 };
